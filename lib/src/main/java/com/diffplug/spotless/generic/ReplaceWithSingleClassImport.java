d06a3cb63cc234b3d75957624e4b412660db1c72
Vincent Potucek <vpotucek@me.com>
2025-06-24 22:20:07 +0200

    ReplaceWithSingleClassImport

--- lib/src/main/java/com/diffplug/spotless/generic/ReplaceWithSingleClassImport.java
+++ lib/src/main/java/com/diffplug/spotless/generic/ReplaceWithSingleClassImport.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 DiffPlug
+ * Copyright 2016-2024 DiffPlug
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,8 @@
 package com.diffplug.spotless.generic;
 
 import java.io.File;
-import java.io.*;
+import java.io.Serializable;
+import java.util.Objects;
 
 import com.diffplug.spotless.FormatterFunc;
 import com.diffplug.spotless.FormatterStep;
@@ -24,21 +25,52 @@ import com.diffplug.spotless.FormatterStep;
 import javax.annotation.Nullable;
 
 public final class ReplaceWithSingleClassImport implements FormatterStep {
+	private static final long serialVersionUID = 1L;
+	private final State state;
+
+	private ReplaceWithSingleClassImport(State state) {
+		this.state = state;
+	}
+
+	public static FormatterStep create(String target, String replacement) {
+		Objects.requireNonNull(target, "target");
+		Objects.requireNonNull(replacement, "replacement");
+		return new ReplaceWithSingleClassImport(new State(target, replacement));
+	}
 
 	@Override
 	public String getName() {
-		return "";
+		return "replaceWithSingleClassImport";
 	}
 
 	@Nullable
 	@Override
 	public String format(String rawUnix, File file) throws Exception {
-		return "";
+		Objects.requireNonNull(rawUnix, "rawUnix");
+		Objects.requireNonNull(file, "file");
+		return state.toFormatter().apply(rawUnix);
 	}
 
 	@Override
 	public void close() throws Exception {
+		// No resources to close
+	}
 
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		ReplaceWithSingleClassImport that = (ReplaceWithSingleClassImport) o;
+		return Objects.equals(state, that.state);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(state);
 	}
 
 	private static final class State implements Serializable {
@@ -54,6 +86,24 @@ public final class ReplaceWithSingleClassImport implements FormatterStep {
 
 		FormatterFunc toFormatter() {
 			return raw -> raw.replace(target, replacement);
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (this == o) {
+				return true;
+			}
+			if (o == null || getClass() != o.getClass()) {
+				return false;
+			}
+			State state = (State) o;
+			return Objects.equals(target, state.target) &&
+				Objects.equals(replacement, state.replacement);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(target, replacement);
 		}
 	}
 }
